90be4ae1ad8cf4efc231fae6410986ee549076ca
Robson <robson@masterhub.com.br>
2019-03-15 14:08:57 -0300

    push

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={					
+	'ApiUrl':"http://integracao.masterhub.com.br/merchantapp/api",       
+	'DialogDefaultTitle':"Delivery System",
+	'APIHasKey':"f0b8922f6edcb9d46f7f33e867e70e7d",
+	'debug': false
+};
