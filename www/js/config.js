1a1cd82aef79bd9cf42e78877ff7c5c031f5a048
Robson Gonçalves <robson@masterhub.com.br>
2019-03-22 22:23:24 -0300

    ...

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={					
+	'ApiUrl':"http://app.masterhub.com.br/merchantapp/api",       
+	'DialogDefaultTitle':"Master Hub®",
+	'APIHasKey':"f0b8922f6edcb9d46f7f33e867e70e7d",
+	'debug': false
+};
