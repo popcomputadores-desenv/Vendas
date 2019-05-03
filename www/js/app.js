53ee2030feae161806611314d7e256bc8b219085
Robson Gonçalves <robson@masterhub.com.br>
2019-05-03 12:50:54 -0300

    ...

--- www/js/app.js
+++ www/js/app.js
@@ -44,11 +44,31 @@ function onDeviceReady() {
 		if(!isDebug()){		
 	 	   getLanguageSettings();
 		}	
-		var data = getDataAtual();
+		
+				var data = new Date();
+		// Guarda cada pedaço em uma variável
+		var dia     = data.getDate();           // 1-31
+		var dia_sem = data.getDay();            // 0-6 (zero=domingo)
+		var mes     = data.getMonth();          // 0-11 (zero=janeiro)
+		var ano2    = data.getYear();           // 2 dígitos
+		var ano4    = data.getFullYear();       // 4 dígitos
+		var hora    = data.getHours();          // 0-23
+		var min     = data.getMinutes();        // 0-59
+		var seg     = data.getSeconds();        // 0-59
+		var mseg    = data.getMilliseconds();   // 0-999
+		var tz      = data.getTimezoneOffset(); // em minutos
+
+		// Formata a data e a hora (note o mês + 1)
+		var str_data = dia + '/' + (mes+1) + '/' + ano4;
+		var str_hora = hora + ':' + min + ':' + seg;
+
+		// Mostra o resultado
+		//alert('Hoje é ' + str_data + ' às ' + str_hora);
+
 		SunmiInnerPrinter.printerStatusStartListener();
 		SunmiInnerPrinter.setFontSize(20);
 		SunmiInnerPrinter.lineWrap(1);
-		SunmiInnerPrinter.printOriginalText("Hoje é "+data); 
+		SunmiInnerPrinter.printOriginalText("Sistema aberto "+ str_data + ' às ' + str_hora); 
 		
 		document.addEventListener("pause", onPause, false);
 		document.addEventListener("resume", onResume, false);
@@ -4394,10 +4414,10 @@ reviewOrder = function(order_id){
 	});
 };
 
-function getDataAtual(data='')
+function getDataAtual(data)
 {
 		// Obtém a data/hora da variável ou atual
-	if (typeof data === "undefined" || data==null || data=="" || data=="null"){
+	if (typeof data == "undefined" || data==null || data=="" || data=="null"){
 		var data = new Date();
 	} else {
 		var data = data;
